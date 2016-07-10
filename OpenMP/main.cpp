64f9a2e9932e9412e03f24e2d3925d845a44663d
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-07-10 23:11:43 +0800

    Use constant.

--- OpenMP/main.cpp
+++ OpenMP/main.cpp
@@ -6,15 +6,15 @@ int main()
 {
     clock_t startTime = clock();
     double sum = 0.0;
-    static long stepNum = 100000000;
+    const long STEP_NUM = 100000000;
     double pi;
-    double stepLength = 1.0 / stepNum;
+    double stepLength = 1.0 / STEP_NUM;
 
     printf("Start calculating...\n");
     double x = 0;
     // computational steps
 #pragma omp parallel for reduction(+:sum)
-    for(int i = 0;i < stepNum; i++)
+    for(int i = 0;i < STEP_NUM; i++)
     {
         x = (i + 0.5) * stepLength;
         sum += 1.0 / (1.0 + x * x);
