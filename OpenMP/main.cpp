a6df40d0f7ab0c8d9b06aa49041b5559f7752af6
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-07-11 11:06:00 +0800

    Change method for calculating time.

--- OpenMP/main.cpp
+++ OpenMP/main.cpp
@@ -1,28 +1,34 @@
-#include<stdio.h>
-#include<time.h>
-#include<omp.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+#include <sys/time.h>
 
-const long STEP_NUM = 100000000;
+const double PI = 3.1415926535897932;
+const long STEP_NUM = 1000000000;
 const double STEP_LENGTH = 1.0 / STEP_NUM;
 
 int main()
 {
-    clock_t startTime = clock();
+    timeval startTime;
+    gettimeofday(&startTime, NULL);
+
     double sum = 0.0;
     double pi;
 
     printf("Start calculating...\n");
     double x = 0;
     // computational steps
-#pragma omp parallel for reduction(+:sum)
+    #pragma omp parallel for reduction(+:sum)
     for(int i = 0;i < STEP_NUM; i++)
     {
         x = (i + 0.5) * STEP_LENGTH;
         sum += 1.0 / (1.0 + x * x);
     }
     pi = STEP_LENGTH * sum * 4;
-    clock_t endTime = clock();
-    printf("PI = %.16lf\nTime elapsed : %lf seconds.\n", pi, (double(endTime - startTime) / CLOCKS_PER_SEC));
+
+    timeval endTime;
+    gettimeofday(&endTime, NULL);
+    printf("PI = %.16lf with error %.16lf\nTime elapsed : %lf seconds.\n", pi, PI - pi, (endTime.tv_sec - startTime.tv_sec) + (double(endTime.tv_usec - startTime.tv_usec) / 10E6 ));
 
     return 0;
 }
