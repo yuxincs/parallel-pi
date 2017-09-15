0b265a75e5f8e3a5d08af6f955593a1d982d9bef
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-09-15 12:43:50 -0400

    Use spaces instead of tabs.

--- cuda.cu
+++ cuda.cu
@@ -11,77 +11,77 @@ const int BLOCK_NUM = 64;
 
 __global__ void integrateSimple(float *sum, int stepNum, float stepLength, int threadNum, int blockNum)
 {
-	int globalThreadId = threadIdx.x + blockIdx.x *  blockDim.x;
-	int start = (stepNum / (blockNum * threadNum)) * globalThreadId;
-	int end = (stepNum / (blockNum * threadNum)) * (globalThreadId + 1);
-	float x;
-	for (int i = start; i < end; i ++)
-	{
-		x = (i + 0.5f) * stepLength;
-		sum[globalThreadId] += 1.0f / (1.0f + x * x);
-	}
+    int globalThreadId = threadIdx.x + blockIdx.x *  blockDim.x;
+    int start = (stepNum / (blockNum * threadNum)) * globalThreadId;
+    int end = (stepNum / (blockNum * threadNum)) * (globalThreadId + 1);
+    float x;
+    for (int i = start; i < end; i ++)
+    {
+        x = (i + 0.5f) * stepLength;
+        sum[globalThreadId] += 1.0f / (1.0f + x * x);
+    }
 }
 
 __global__ void integrateOptimised(float *globalSum, int stepNum, float stepLength, int threadNum, int blockNum)
 {
-	int globalThreadId = threadIdx.x + blockIdx.x * blockDim.x;
-	int start = (stepNum / (blockNum * threadNum)) * globalThreadId;
-	int end = (stepNum / (blockNum * threadNum)) * (globalThreadId + 1);
-	int localThreadId = threadIdx.x;
-	int blockId = blockIdx.x;
+    int globalThreadId = threadIdx.x + blockIdx.x * blockDim.x;
+    int start = (stepNum / (blockNum * threadNum)) * globalThreadId;
+    int end = (stepNum / (blockNum * threadNum)) * (globalThreadId + 1);
+    int localThreadId = threadIdx.x;
+    int blockId = blockIdx.x;
 
-	// shared memory to hold the sum for each block
-	__shared__ float blockSum[THREAD_NUM];
+    // shared memory to hold the sum for each block
+    __shared__ float blockSum[THREAD_NUM];
 
-	memset(blockSum, 0, threadNum * sizeof(float));
+    memset(blockSum, 0, threadNum * sizeof(float));
 
-	float x;
-	for (int i = start; i < end; i ++) 
-	{
-		x = (i + 0.5f) * stepLength;
-		blockSum[localThreadId] += 1.0f / (1.0f+ x * x);
-	}
-	blockSum[localThreadId] *= stepLength * 4;
+    float x;
+    for (int i = start; i < end; i ++)
+    {
+        x = (i + 0.5f) * stepLength;
+        blockSum[localThreadId] += 1.0f / (1.0f+ x * x);
+    }
+    blockSum[localThreadId] *= stepLength * 4;
 
-	// wait for all threads to catch up
-	__syncthreads();
+    // wait for all threads to catch up
+    __syncthreads();
 
-	// for each block, do sum using shared memory
-	for (int i = blockDim.x / 2; i > 0; i >>= 1)
-	{ 
-		if (localThreadId < i)
-			blockSum[localThreadId] += blockSum[localThreadId + i];
+    // for each block, do sum using shared memory
+    for (int i = blockDim.x / 2; i > 0; i >>= 1)
+    {
+        if (localThreadId < i)
+            blockSum[localThreadId] += blockSum[localThreadId + i];
 
-		__syncthreads();
-	}
+        __syncthreads();
+    }
 
-	// sum up the summation of the block and write to the global sum
-	if(localThreadId == 0)
-		globalSum[blockId] = blockSum[0];
+    // sum up the summation of the block and write to the global sum
+    if(localThreadId == 0)
+        globalSum[blockId] = blockSum[0];
 }
 
 // parallel reduction to speedup summation which can only be performed inside a block
 __global__ void sumReduce(float *sum, float *sumArray, int arraySize)
 {
-	int localThreadId = threadIdx.x;
-	for (int i = blockDim.x / 2; i > 0; i >>= 1)
-	{ 
-		if (localThreadId < i)
-			sumArray[localThreadId] += sumArray[localThreadId + i];
+    int localThreadId = threadIdx.x;
+    for (int i = blockDim.x / 2; i > 0; i >>= 1)
+    {
+        if (localThreadId < i)
+            sumArray[localThreadId] += sumArray[localThreadId + i];
 
-		__syncthreads();
-	}
+        __syncthreads();
+    }
 
-	if(localThreadId == 0)
-		*sum = sumArray[0];
+    if(localThreadId == 0)
+        *sum = sumArray[0];
 }
 
 int main()
 {
-	int deviceCount = 0;
+    int deviceCount = 0;
 
-	printf("\nConfiguring device...\n");
-    
+    printf("\nConfiguring device...\n");
+
     cudaError_t error = cudaGetDeviceCount(&deviceCount);
 
     if (error != cudaSuccess)
@@ -90,96 +90,96 @@ int main()
         return 1;
     }
 
-	if(deviceCount == 0)
-	{
-		printf("There are no available CUDA device(s)\n");
-		return 1;
-	}
-	else
-		printf("%d CUDA Capable device(s) detected\n", deviceCount);
+    if(deviceCount == 0)
+    {
+        printf("There are no available CUDA device(s)\n");
+        return 1;
+    }
+    else
+        printf("%d CUDA Capable device(s) detected\n", deviceCount);
 
-	/* Simple Calculation (without optimization) */
-	dim3 block(THREAD_NUM);
-	dim3 grid(BLOCK_NUM);
-	float *hostSum = NULL, *deviceSum = NULL;
-	float piSimple = 0;
-	
-	// allocate host memory
-	hostSum = (float *)malloc(BLOCK_NUM * THREAD_NUM * sizeof(float));	
+    /* Simple Calculation (without optimization) */
+    dim3 block(THREAD_NUM);
+    dim3 grid(BLOCK_NUM);
+    float *hostSum = NULL, *deviceSum = NULL;
+    float piSimple = 0;
 
-	// allocate device memory
-	cudaMalloc((void **) &deviceSum, BLOCK_NUM * THREAD_NUM * sizeof(float));
+    // allocate host memory
+    hostSum = (float *)malloc(BLOCK_NUM * THREAD_NUM * sizeof(float));
 
-	// clear device memory
-	cudaMemset(deviceSum, 0, BLOCK_NUM * THREAD_NUM * sizeof(float));
+    // allocate device memory
+    cudaMalloc((void **) &deviceSum, BLOCK_NUM * THREAD_NUM * sizeof(float));
 
-	// CUDA events needed to measure execution time
-	cudaEvent_t startTime, stopTime;
-	float simpleGpuTime, optimizedGpuTime;
+    // clear device memory
+    cudaMemset(deviceSum, 0, BLOCK_NUM * THREAD_NUM * sizeof(float));
 
-	// start timer
-	cudaEventCreate(&startTime);
-	cudaEventCreate(&stopTime);
-	cudaEventRecord(startTime, 0);
+    // CUDA events needed to measure execution time
+    cudaEvent_t startTime, stopTime;
+    float simpleGpuTime, optimizedGpuTime;
 
-	printf("Start calculating in simple kernel function...\n");
-	integrateSimple<<<grid, block>>>(deviceSum, STEP_NUM, STEP_LENGTH, THREAD_NUM, BLOCK_NUM);	
+    // start timer
+    cudaEventCreate(&startTime);
+    cudaEventCreate(&stopTime);
+    cudaEventRecord(startTime, 0);
 
-	// stop timer
-	cudaEventRecord(stopTime, 0);
-	cudaEventSynchronize(stopTime);
-	cudaEventElapsedTime(&simpleGpuTime, startTime, stopTime);
+    printf("Start calculating in simple kernel function...\n");
+    integrateSimple<<<grid, block>>>(deviceSum, STEP_NUM, STEP_LENGTH, THREAD_NUM, BLOCK_NUM);
 
-	// retrieve result from device
-	cudaMemcpy(hostSum, deviceSum, BLOCK_NUM * THREAD_NUM * sizeof(float), cudaMemcpyDeviceToHost);
-	
-	// sum result on host
-	for (int i = 0;i < BLOCK_NUM * THREAD_NUM; i++)
-		piSimple += hostSum[i];	
-	
-	piSimple *= STEP_LENGTH * 4;
-	printf("PI = %.16lf with error %.16lf\nTime elapsed : %f seconds.\n\n", piSimple, fabs(piSimple - PI), simpleGpuTime / 1000);
+    // stop timer
+    cudaEventRecord(stopTime, 0);
+    cudaEventSynchronize(stopTime);
+    cudaEventElapsedTime(&simpleGpuTime, startTime, stopTime);
 
-	free(hostSum);
-	cudaFree(deviceSum);
+    // retrieve result from device
+    cudaMemcpy(hostSum, deviceSum, BLOCK_NUM * THREAD_NUM * sizeof(float), cudaMemcpyDeviceToHost);
 
-	/* Optimized Calculation */
+    // sum result on host
+    for (int i = 0;i < BLOCK_NUM * THREAD_NUM; i++)
+        piSimple += hostSum[i];
+
+    piSimple *= STEP_LENGTH * 4;
+    printf("PI = %.16lf with error %.16lf\nTime elapsed : %f seconds.\n\n", piSimple, fabs(piSimple - PI), simpleGpuTime / 1000);
+
+    free(hostSum);
+    cudaFree(deviceSum);
+
+    /* Optimized Calculation */
     float pi = 0.0;
     float *deviceBlockSum;
-	float *devicePi;
-	//float *hostBlockSum;
- 
-	// allocate memory on host
-	//hostBlockSum = (float *)malloc(BLOCK_NUM * sizeof(float));
+    float *devicePi;
+    //float *hostBlockSum;
+
+    // allocate memory on host
+    //hostBlockSum = (float *)malloc(BLOCK_NUM * sizeof(float));
 
     // allocate memory on GPU
-	cudaMalloc((void **) &devicePi, sizeof(float));
+    cudaMalloc((void **) &devicePi, sizeof(float));
     cudaMalloc( (void **) &deviceBlockSum, sizeof(float) * BLOCK_NUM);
 
-	// Start timer
-	cudaEventRecord(startTime, 0);
-	printf("Start calculating in optimized kernel function...\n");
-	integrateOptimised<<<BLOCK_NUM, THREAD_NUM>>>(deviceBlockSum, STEP_NUM, STEP_LENGTH, THREAD_NUM, BLOCK_NUM);
-	sumReduce<<<1, BLOCK_NUM>>>(devicePi, deviceBlockSum, BLOCK_NUM);
+    // Start timer
+    cudaEventRecord(startTime, 0);
+    printf("Start calculating in optimized kernel function...\n");
+    integrateOptimised<<<BLOCK_NUM, THREAD_NUM>>>(deviceBlockSum, STEP_NUM, STEP_LENGTH, THREAD_NUM, BLOCK_NUM);
+    sumReduce<<<1, BLOCK_NUM>>>(devicePi, deviceBlockSum, BLOCK_NUM);
 
-	// retrieve result from device
-	cudaMemcpy(&pi, devicePi, sizeof(float), cudaMemcpyDeviceToHost);
-	//cudaMemcpy(hostBlockSum, deviceBlockSum, BLOCK_NUM * sizeof(float), cudaMemcpyDeviceToHost);
-	
-	// sum result on host
-	//for (int i = 0;i < BLOCK_NUM; i++)
-		//pi += hostBlockSum[i];	
+    // retrieve result from device
+    cudaMemcpy(&pi, devicePi, sizeof(float), cudaMemcpyDeviceToHost);
+    //cudaMemcpy(hostBlockSum, deviceBlockSum, BLOCK_NUM * sizeof(float), cudaMemcpyDeviceToHost);
 
-	cudaEventRecord(stopTime, 0);
-	cudaEventSynchronize(stopTime);
-	cudaEventElapsedTime(&optimizedGpuTime, startTime, stopTime);
+    // sum result on host
+    //for (int i = 0;i < BLOCK_NUM; i++)
+        //pi += hostBlockSum[i];
 
-	printf("PI = %.16lf with error %.16lf\nTime elapsed : %f seconds.\n\n", pi, fabs(pi - PI), optimizedGpuTime / 1000);
+    cudaEventRecord(stopTime, 0);
+    cudaEventSynchronize(stopTime);
+    cudaEventElapsedTime(&optimizedGpuTime, startTime, stopTime);
 
-	// free memory
-	cudaFree(deviceBlockSum);
+    printf("PI = %.16lf with error %.16lf\nTime elapsed : %f seconds.\n\n", pi, fabs(pi - PI), optimizedGpuTime / 1000);
 
-	// reset Device
-	cudaDeviceReset();
-	return 0;
+    // free memory
+    cudaFree(deviceBlockSum);
+
+    // reset Device
+    cudaDeviceReset();
+    return 0;
 }
