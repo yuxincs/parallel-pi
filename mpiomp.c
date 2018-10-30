9c7157363c03f235d5332ba3d4194558c73efa52
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-10-29 23:41:29 -0400

    Combine MPI and openmp.

--- mpiomp.c
+++ mpiomp.c
@@ -7,11 +7,12 @@
 #include <omp.h>
 
 const double PI = 3.1415926535897932;
-const long STEP_NUM = 10000000;
+const long STEP_NUM = 1070596096;
+const double STEP_LENGTH = 1.0 / 1070596096;
 
 int main (int argc, char* argv[])
 {
-  long totalCount = 0;
+  double pi, sum = 0.0;
 
   MPI_Init (&argc, &argv);
 
@@ -30,20 +31,18 @@ int main (int argc, char* argv[])
 
   double startTime = MPI_Wtime();
 
-  long count = 0;
-  srand((int)time(NULL) ^ omp_get_thread_num());
-  // each process will calculate a part of the sum
-  #pragma omp parallel for reduction(+:count) num_threads(4)
-  for (int i = 0; i < STEP_NUM / size; i ++)
+  double result = 0.0;
+  double x;
+  // each process will caculate a part of the sum
+  #pragma omp parallel for reduction(+:result) private(x) num_threads(4)
+  for (int i = rank * STEP_NUM / size; i < rank * STEP_NUM / size + STEP_NUM / size; i ++)
   {
-    double x = (double)rand() / RAND_MAX;
-    double y = (double)rand() / RAND_MAX;
-    if ((x * x + y * y) <= 1)
-      count += 1;
+    x = (i + 0.5) * STEP_LENGTH;
+    result += 1.0 / (1.0 + x * x);
   }
 
   // sum up all results
-  MPI_Reduce(&count, &totalCount, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(&result, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
   // synchronize all processes and get the end time
   MPI_Barrier(MPI_COMM_WORLD);
@@ -52,9 +51,9 @@ int main (int argc, char* argv[])
   // caculate and print PI
   if (rank == 0)
   {
-    double pi = ((double)totalCount / STEP_NUM) * 4;
-    printf("PI = %.16lf with error %.16lf\nTime elapsed : %lf seconds.\n\n", pi, PI - pi, (endTime - startTime));
-    assert(fabs(PI - pi) <= 0.001);
+    pi = STEP_LENGTH * sum * 4;
+    printf("PI = %.16lf with error %.16lf\nTime elapsed : %lf seconds.\n\n", pi, fabs(pi - PI), (endTime - startTime));
+    assert(fabs(pi - PI) <= 0.001);
   }
 
   MPI_Finalize();
